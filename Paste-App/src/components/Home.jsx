3b178d6447f1e30fc4a24f0224d47b8eae0c3c82
AdnanBarud <barudyt@gmail.com>
2025-03-01 11:55:51 +0530

    create textarea, createPaste function, style button, textarea

--- Paste-App/src/components/Home.jsx
+++ Paste-App/src/components/Home.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { use, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pasteId=searchParams.get('pasteId');
 
-export default Home
+
+  function createPaste() {
+    const paste = {
+      title: title,
+      content: value,
+      _id: pasteId ||
+        Date.now().toString(33),
+      createdAt: new Date().toISOString(),
+
+    }
+  }
+
+  return (
+  <div>
+    <div className="flex justify-evenly items-center gap-x-4">
+
+      {/* for title */}
+      <div className=" ">
+        <input
+          className="rounded-2xl p-2 mt-2 bg-[#0f0c0c] w-[300px]"
+          value={title}
+          type="text"
+          placeholder="enter title here"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+
+      {/* button for update or create */}
+      <button
+      className="rounded-2xl p-2 mt-2 "
+      onClick={createPaste}
+      >
+        {
+            pasteId ? 'Update My Paste' : 'Create My Paste'
+        }
+      </button>
+
+
+      {/* for content input */}
+      
+    </div>
+
+    <div>
+        <textarea 
+        className="rounded-2xl p-2 mt-2 bg-[#0f0c0c] w-[300px] rows-20"
+        value={value}
+        onChange={(e)=> setValue(e.target.value)}
+        >
+          
+        </textarea>
+      </div>
+  </div>
+  );
+};
+
+export default Home;
--- Paste-App/src/components/Navbar.jsx
+++ Paste-App/src/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex gap-2 justify-center'>
+      <NavLink
+      to='/'
+      >
+        Home
+      </NavLink>
+      <NavLink
+      to='/pastes'
+      >
+        Pastes
+      </NavLink>
+    </div>
   )
 }
 
